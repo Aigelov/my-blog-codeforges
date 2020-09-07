b628308e31268c60597cb41aed9ac386c51212f2
Aigelov <aigelov@gmail.com>
2020-09-07 12:06:35 +0600

    First commit

---
+++ src/app/blog/blog.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { PostListModule } from './post-list/post-list.module';
+
+
+@NgModule({
+    imports: [
+      PostListModule
+    ],
+    exports: [],
+    declarations: [],
+    providers: [],
+})
+export class BlogModule {
+}
---
+++ src/app/blog/post-list/post-list.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { PostListComponent } from './post-list.component';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [PostListComponent],
+  providers: [],
+})
+export class PostListModule {
+}
